6beaf4910c7f76ab9153e076efc7162f48fdc6f4
MandukoGR <gurom84@outlook.com>
2024-10-16 08:20:38 -0600

    add unit tests

--- server/urls.py
+++ server/urls.py
@@ -2,11 +2,11 @@ from django.urls import re_path
 from api import views
 
 urlpatterns = [
-    re_path('login', views.login),
-    re_path('signup', views.signup),
-    re_path('test_token', views.test_token),
-    re_path('refresh_token', views.refresh_token),
-    re_path('products', views.create_product),
+    re_path('login', views.login, name='login'),
+    re_path('signup', views.signup, name='signup'),
+    re_path('test_token', views.test_token, name='test_token'),
+    re_path('refresh_token', views.refresh_token, name='refresh_token'),
+    re_path('products', views.create_product, name='create_product'),
 ]
 
 
